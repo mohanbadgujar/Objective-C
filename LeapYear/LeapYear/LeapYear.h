fc901dacaafb00fd34b86aea358ab7fee587807c
mohanbadgujar <badgujarmohan10@gmail.com>
2017-02-22 15:55:43 +0530

    3rd

---
+++ LeapYear/LeapYear/LeapYear.h
@@ -0,0 +1,17 @@
+//
+//  LeapYear.h
+//  LeapYear
+//
+//  Created by BridgeLabz Solutions LLP  on 2/7/17.
+//  Copyright © 2017 BridgeLabz Solutions LLP . All rights reserved.
+//
+
+@interface LeapYear : NSObject
+{
+    int year;
+    int temp;
+    int count;
+}
+-(void)GetInput;
+-(void)calculate;
+@end
